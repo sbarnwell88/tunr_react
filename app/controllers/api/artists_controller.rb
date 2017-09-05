3679fa4a38ede1fb32be64acd2b72e33369d4c24
sbarnwell88 <susy6688@gmail.com>
2017-09-05 12:22:25 -0400

    finished controllers

---
+++ app/controllers/api/artists_controller.rb
@@ -0,0 +1,49 @@
+class Api::ArtistsController < ApplicationController
+    def index
+        @artists = Artist.all 
+        render json: @artists 
+    end
+
+    def show
+        @artist = Artist.find params[:id]
+        render json: @artist
+    end
+
+    def create
+        @artist = Artist.new artist_params
+
+        if @artist.save
+            render json: @artist
+        else
+            render json: {
+                message: 'Error when creating artist'
+            }
+        end
+    end
+
+    def update
+        @artist = Artist.find params[:id]
+
+        if @artist.update(artist_params)
+            render json: @artist
+        else
+            render json: {
+                message: 'Error when creating artist'
+            }
+        end
+    end
+
+    def destroy
+        @artist = Artist.find params[:id]
+        @artist.destroy
+
+        render json: {
+            message: 'Artist successfully destroyed'
+        }
+    end
+
+    private 
+    def artist_params
+        params.require(:artist).permit(:name, :photo_url, :nationality)
+    end
+end
